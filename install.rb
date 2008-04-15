f0188f9adfcd281f5117cd14559e3a8e82c365cc
Robby Russell <robby@planetargon.com>
2008-04-14 23:04:23 -0700

    gsub not replace...

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 def copy(file_name, from_dir, to_dir)
   FileUtils.mkdir to_dir unless File.exist?(File.expand_path(to_dir))   
   from = File.expand_path(File.join(from_dir,file_name))
-  to = File.expand_path(File.join(to_dir, file_name.replace('.example', '')))
+  to = File.expand_path(File.join(to_dir, file_name.gsub('.example', '')))
   FileUtils.cp from, to, :verbose => true unless File.exist?(to)
 end
 
