2ba17bfae33c6ae3e9a0aa893c34108120a46d5a
Alex Malinovich <alexmalinovich@gmail.com>
2008-04-17 17:12:15 -0700

    * Fixed install.rb to say "boxcar:config" instead of "boxcar:setup"

--- install.rb
+++ install.rb
@@ -16,7 +16,7 @@ begin
   puts "# Boxcar Conductor -- INSTALLED! "
   puts "#   Next steps:"
   puts "#     1. Edit config/deploy.rb"
-  puts "#     2. Run cap boxcar:setup"  
+  puts "#     2. Run cap boxcar:config"  
   puts "#############################################"  
 rescue Exception => e
   puts "There are problems copying Boxcar configuration files to you app: #{e.message}"
