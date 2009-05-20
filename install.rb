110fe9b41edfa989ebdc66306a479f99d3a8b63d
Alex Malinovich <alex.malinovich@planetargon.com>
2009-05-20 15:37:14 -0700

    Cleaning up whitespace (and adding pre-autodb tag)

--- install.rb
+++ install.rb
@@ -1,12 +1,12 @@
 def copy(file_name, from_dir, to_dir)
-  FileUtils.mkdir to_dir unless File.exist?(File.expand_path(to_dir))   
+  FileUtils.mkdir to_dir unless File.exist?(File.expand_path(to_dir))
   from = File.expand_path(File.join(from_dir,file_name))
   to = File.expand_path(File.join(to_dir, file_name.gsub('.example', '')))
   FileUtils.cp from, to, :verbose => true unless File.exist?(to)
 end
 
 # copy config files to application directory
-begin 
+begin
   templates_dir = File.join(File.dirname(__FILE__), 'templates')
   config_dir = File.join(RAILS_ROOT, 'config')
   root_dir = File.join(RAILS_ROOT)
@@ -17,7 +17,7 @@ begin
   puts "#   Next steps:"
   puts "#     1. Edit config/deploy.rb"
   puts "#     2. Run cap boxcar:config -q"
-  puts "#############################################"  
+  puts "#############################################"
 rescue Exception => e
   puts "There are problems copying Boxcar configuration files to you app: #{e.message}"
 end
